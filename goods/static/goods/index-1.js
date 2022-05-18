4fdb68a8b2f0b73b0fc785111ae287cec4ac1bbf
AlekanderOst <88969784+AlekanderOst@users.noreply.github.com>
2022-05-18 17:24:08 +0300

    commit-1

---
+++ goods/static/goods/index-1.js
@@ -0,0 +1,13 @@
+MyMenu.onclick = function My_function() {
+    var x = document.getElementById("MyButton");
+    var y = document.getElementById("NoneMenu");
+    if(x.className === "button"
+
+    ) {
+        x.className += " responsive";
+        y.className += " responsive";
+    } else {
+        x.className === "button"
+        y.className === "plase_button"
+    }
+}
